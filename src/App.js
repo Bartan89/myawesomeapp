525b879a757c9dc336155588eb2bb6377a3af1c6
Bartan89 <bart.kuijper89@gmail.com>
2020-07-20 12:07:41 +0200

    build optimised

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bart's utterly amazing app
         </a>
       </header>
     </div>
